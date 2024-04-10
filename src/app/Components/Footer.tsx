050b2fbe8c4b098f00264e187024777ce55c0610
Switch <thunyoubun@gmail.com>
2024-04-11 02:59:03 +0700

    edite foooter

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -3,7 +3,7 @@ import { FaPhone } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div className="bg-white h-44 flex justify-center">
-      <div className="m-10 flex flex-col gap-2 items-center justify-center">
+      <div className="m-10 flex flex-col gap-4 items-center justify-center">
         <div className="flex gap-2">
           <div className=" bg-red-600 w-16 h-16"></div>
           <div className="w-fit flex flex-col">
@@ -19,9 +19,11 @@ const Footer = () => {
             15th International Conference on Information Technology and
             Electrical Engineering
           </h1>
-          <h2 className="flex justify-center">
+        </div>
+        <div className=" text-center text-sm">
+          <h1 className="flex justify-center">
             <FaPhone size={25} className=" inline" /> (+66) 2-723-4900
-          </h2>
+          </h1>
         </div>
       </div>
     </div>
