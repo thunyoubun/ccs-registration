473332d4ac7075d732cd7bc66e4e1eef394ba549
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-04-12 12:10:07 +0700

    implement card sponsor

--- src/app/Components/CardSponsor.tsx
+++ src/app/Components/CardSponsor.tsx
@@ -1,7 +1,15 @@
-function CardSponsor(){
+import Image from "next/image"
+
+function CardSponsor({ path, nameCompany1, nameCompany2}:{ path:string, nameCompany1:string, nameCompany2:string}){
     return(
-        <div>
-            
+        <div className="max-w-sm p-6 bg-white grid justify-items-center">
+            <Image src={path} alt={nameCompany1} width={128} height={128}/>
+            <div className="text-center">
+                <h5 className="text-xl font-medium">{nameCompany1}</h5>
+                <h5 className="text-base font-light">{nameCompany2}</h5>
+            </div>
         </div>
     )
 }
+
+export default CardSponsor
