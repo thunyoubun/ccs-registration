f90e400cd434a00e3dff7526ac47d97e815a0d96
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-04-13 22:50:24 +0700

    add footer and replace data

--- src/app/programme/page.tsx
+++ src/app/programme/page.tsx
@@ -1,4 +1,7 @@
+'use client'
+import Footer from "../Components/Footer"
 import Navbar from "../Components/Navbar"
+import TableProgramme from "../Components/TableProgramme"
 
 function ProgrammePage() {
     return (
@@ -8,41 +11,10 @@ function ProgrammePage() {
                 <div className="mt-24 p-16 bg-gradient-to-r from-red-700 to-blue-800 flex justify-center ">
                     <div className="grid grid-cols-1 gap-7">
                         <h2 className="text-white text-5xl font-semibold text-center">Programme</h2>
-                        <div className="bg-white grid-cols-full rounded">
-                            <table className="table-auto">
-                                <thead>
-                                    <tr>
-                                        <th>TIME</th>
-                                        <th>GRAND BALLROOM</th>
-                                        <th>SPEAKERs</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>08.30 - 09.00</td>
-                                        <td>Registration and welcome refreshment</td>
-                                    </tr>
-                                    <tr>
-                                        <td>09.00 - 09.30</td>
-                                        <td>Opening ceremony</td>
-                                    </tr>
-                                    <tr>
-                                        <td>09.30 - 09.45</td>
-                                        <td>Carbon Neutrality Research and Innovation</td>
-                                    </tr>
-                                    <tr>
-                                        <td>09.45 - 10.15</td>
-                                        <td>Science, Research, and Innovation Toward Net-Zero via CCUS</td>
-                                    </tr>
-                                    <tr>
-                                        <td>10.15 - 10.45</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                        <TableProgramme />
                     </div>
-
                 </div>
+                <Footer />
             </div>
         </header>
     )
