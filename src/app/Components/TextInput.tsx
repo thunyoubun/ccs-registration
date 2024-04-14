c54b972f3ee28a62de23f7c911723c175a0e2ea1
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-04-14 18:04:28 +0700

    delete block

--- src/app/Components/TextInput.tsx
+++ src/app/Components/TextInput.tsx
@@ -7,7 +7,7 @@ function TextInput(fnc: Function, word: string, tagName: string, typeInput: stri
                     type={typeInput} 
                     id={tagName}
                     {...fnc(tagName)}
-                    className="block w-full p-1 rounded-md border-0 shadow-sm ring-1 ring-inset ring-rose-700 focus:ring-2 focus:ring-blue-900"
+                    className=" w-full p-1 rounded-md border-0 shadow-sm ring-1 ring-inset ring-rose-700 focus:ring-2 focus:ring-blue-900"
                     required
                     />
             </div>
