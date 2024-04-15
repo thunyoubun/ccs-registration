d13f1f785f907cdd202bb1008d257494d2f0f882
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-04-15 11:04:46 +0700

    add show speaker, penalist, and facilitator

--- src/app/Components/TableProgramme.tsx
+++ src/app/Components/TableProgramme.tsx
@@ -7,31 +7,74 @@ function RowData() {
         console.log("Expand")
     }
     return ScheduleData["programme-schedule"].map((schedule) =>
-        <tr key={schedule.topic + schedule.time} className=" hover:bg-gray-100" onClick={() => schedule.bgColor? {}: ExpandRowData()}>
-            <td className="font-light text-xl text-center p-3">{schedule.time}</td>
-            <td>
-                <h3 className="font-medium text-xl">{schedule.topic}</h3>
-                {schedule.description.map((dest, index) =>
-                    <h4 key={dest}>{dest}</h4>
-                )}
-            </td>
-            <td className="flex justify-center content-center">
-                <MdOutlineExpandLess />
-            </td>
-        </tr>
+        <>
+            <tr key={schedule.topic + schedule.time} className={`${schedule.bgColor ? "bg-rose-700 shadow rounded-lg" : " hover:bg-gray-100"} shadow rounded-lg`} onClick={() => schedule.bgColor ? {} : ExpandRowData()}>
+                <td className="font-light text-lg text-center p-2">{schedule.time}</td>
+                <td>
+                    <h3 className="font-medium text-lg ">{schedule.topic}</h3>
+                </td>
+                <td>{schedule.group}</td>
+                <td className="flex justify-center content-center">
+                    <button className="content-center">
+                        <MdOutlineExpandLess />
+                    </button>
+                </td>
+            </tr>
+            {schedule.description.length > 0 || schedule.speakers.length > 0 || schedule.panelists.length > 0 || schedule.facilitators.length ?
+                <tr className="bg-gray-200 rounded-b-lg">
+                    <td></td>
+                    <td className="grid gap-2">
+                        {schedule.description.length > 0 ? 
+                            <div>
+                                {schedule.description.map((data)=>
+                                    <h2 key={data}>{data}</h2>
+                                )}
+                            </div>
+                        : ''}
+                        {schedule.speakers.length > 0 ?
+                            <div>
+                                <h2 className="font-semibold">Speaker:</h2>
+                                {schedule.speakers.map((data) =>
+                                    <h2 key={data}>{data}</h2>
+                                )}
+                            </div>
+                            : ''}
+                        {schedule.panelists.length > 0 ?
+                            <div>
+                                <h2 className="font-semibold">Panelists:</h2>
+                                {schedule.panelists.map((data) =>
+                                    <h2 key={data}>{data}</h2>
+                                )}
+                            </div> :
+                            ''
+                        }{schedule.facilitators.length > 0 ?
+                            <div>
+                                <h2 className="font-semibold">Facilitator:</h2>
+                                {schedule.facilitators.map((data) =>
+                                    <h2 key={data}>{data}</h2>
+                                )}
+                            </div>
+                            : ''}
+                    </td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                : <></>}
+        </>
     )
 }
 
 function TableProgramme() {
 
     return (
-        <div className="bg-white grid-cols-full rounded shadow-lg overflow-auto">
-            <table className="w-full table-auto">
+        <div className="bg-white grid-cols-1 rounded shadow-lg overflow-auto">
+            <table className="table-auto mx-2 my-2">
                 <thead className="">
                     <tr className="border-b-2">
-                        <th className="text-center text-2xl">TIME</th>
-                        <th className="text-center text-2xl">AGENDA</th>
-                        <th className="text-center text-2xl">ACTION</th>
+                        <th className="text-center font-medium text-2xl">TIME</th>
+                        <th className="text-center font-medium text-2xl">AGENDA</th>
+                        <th className="text-center font-medium text-2xl">GROUP</th>
+                        <th className="text-center font-medium text-2xl">ACTION</th>
                     </tr>
                 </thead>
                 <tbody>
