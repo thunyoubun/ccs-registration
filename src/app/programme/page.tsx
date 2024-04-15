8b2c655371058b9530304ba8cbbd4beed9ef23ad
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-04-15 18:40:08 +0700

    use navbar programme

--- src/app/programme/page.tsx
+++ src/app/programme/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 import Footer from "../Components/Footer"
-import Navbar from "../Components/Navbar"
+import NavbarProgramme from "../Components/NavbarProgramme"
 import TableProgramme from "../Components/TableProgramme"
 
 function ProgrammePage() {
     return (
         <header>
             <div className="h-screen relative">
-                <Navbar />
+                <NavbarProgramme />
                 <div className="mt-24 p-16 bg-gradient-to-r from-red-700 to-blue-800 flex justify-center ">
                     <div className="grid grid-cols-1 gap-7">
                         <h2 className="text-white text-5xl font-semibold text-center">Programme</h2>
