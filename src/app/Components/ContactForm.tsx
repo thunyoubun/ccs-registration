c7ca697ead955de9ad94879a56cf68bdcd535cc3
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-04-16 23:21:38 +0700

    create form for contact

---
+++ src/app/Components/ContactForm.tsx
@@ -0,0 +1,33 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+export interface IContactForm{
+    name: string,
+    email: string,
+    subject: string,
+    message: string
+}
+
+const validationContactForm = z.object({
+    name: z.string(),
+    email: z.string(),
+    subject: z.string(),
+    message: z.string()
+})
+
+function ContactForm(){
+    const form = useForm<IContactForm>({
+        resolver: zodResolver(validationContactForm),
+        defaultValues:{
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
+        }
+    })
+
+    return form
+}
+
+export default ContactForm
