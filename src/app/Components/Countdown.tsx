54398634be19ab20b6157b229a4f0e0e162d179f
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-04-18 17:18:47 +0700

    organize text

--- src/app/Components/Countdown.tsx
+++ src/app/Components/Countdown.tsx
@@ -10,33 +10,36 @@ function Countdown({ targetDate }: { targetDate: Date }) {
   }, []);
   return (
     <div className="p-4 bg-red-700 rounded-t-lg text-white text-center gap-3 lg:flex lg:justify-center">
-      <h1 className="text-3xl font-medium content-center md:text-5xl">
-        TIME TO END -
-      </h1>
+      <div className="flex flex-col">
+        <h1 className="text-xl font-medium content-center md:text-5xl">
+          Deadline -
+        </h1>
+        <h4 className="">(3 June 2024)</h4>
+      </div>
       <div className="flex text-center gap-3 justify-center">
         <div>
-          <h1 className="font-semibold text-4xl lg:text-6xl">
+          <h1 className="font-semibold text-xl lg:text-6xl">
             {day && firstLoading ? day : ""}
           </h1>
           <h4 className="">day(s)</h4>
         </div>
-        <h1 className="font-semibold text-4xl lg:text-6xl">:</h1>
+        <h1 className="font-semibold text-xl lg:text-6xl">:</h1>
         <div>
-          <h1 className="font-semibold text-4xl lg:text-6xl">
+          <h1 className="font-semibold text-xl lg:text-6xl">
             {hour && firstLoading ? hour : ""}
           </h1>
           <h4>hour(s)</h4>
         </div>
-        <h1 className="font-semibold text-4xl lg:text-6xl">:</h1>
+        <h1 className="font-semibold text-xl lg:text-6xl">:</h1>
         <div>
-          <h1 className="font-semibold text-4xl lg:text-6xl">
+          <h1 className="font-semibold text-xl lg:text-6xl">
             {minute && firstLoading ? minute : ""}
           </h1>
           <h4>minute(s)</h4>
         </div>
-        <h1 className="font-semibold text-4xl lg:text-6xl">:</h1>
+        <h1 className="font-semibold text-xl lg:text-6xl">:</h1>
         <div>
-          <h1 className="font-semibold text-4xl lg:text-6xl">
+          <h1 className="font-semibold text-xl lg:text-6xl">
             {second && firstLoading ? second : ""}
           </h1>
           <h4>second(s)</h4>
