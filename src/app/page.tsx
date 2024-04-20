6f47eb8c024e90ea8368e10bf1b8957b574ad86e
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-04-20 16:05:00 +0700

    add route for register data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import { FaArrowRightLong } from "react-icons/fa6";
 
@@ -12,8 +12,12 @@ import Direction from "./Screens/Direction";
 import Footer from "./Components/Footer";
 
 import { ScrollToSection } from "./Functionalitys/ScrollTo";
+import axios from "axios";
 
 export default function Home() {
+  useEffect(()=>{
+  
+  },[])
   return (
     <div className="relative">
       <Navbar path={"/"} />
