a5fb49d4f87ee3274b3bc893cc977cafd1e6e802
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-04-20 16:06:21 +0700

    delete useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
-<<<<<<< HEAD
-import React, { useEffect } from "react";
-import Link from "next/link";
-import { FaArrowRightLong } from "react-icons/fa6";
-=======
 import React from "react";
->>>>>>> e06fe619384913c0e49c0ae1347c34cd7f10ac60
 
 import Navbar from "./Components/Navbar";
 import ToTop from "./Components/ToTop";
@@ -17,9 +11,6 @@ import Direction from "./Screens/Direction";
 import Footer from "./Components/Footer";
 
 export default function Home() {
-  useEffect(()=>{
-  
-  },[])
   return (
     <div className="relative">
       <Navbar path={"/"} />
