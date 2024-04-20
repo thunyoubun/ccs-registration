1126d478ca44370f46a13c5f6796fa2248be8382
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-04-20 18:42:08 +0700

    clean code

---
+++ src/app/keynote-speaker/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../Components/Navbar"
+import ToTop from "../Components/ToTop"
+
+function KeynoteSpeakerPage(){
+    return(
+        <header>
+            <div className="relative">
+                <Navbar path={"/"} />
+                <ToTop />
+            </div>
+        </header>
+    )
+}
+
+export default KeynoteSpeakerPage
