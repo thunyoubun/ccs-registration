dda9a7c85e149bab59ead0a83f7a041ac9da2aa5
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-04-22 00:24:47 +0700

    create kenote page

--- src/app/keynote-speaker/page.tsx
+++ src/app/keynote-speaker/page.tsx
@@ -7,6 +7,9 @@ function KeynoteSpeakerPage(){
             <div className="relative">
                 <Navbar path={"/"} />
                 <ToTop />
+                <div className="mt-28 bg-gradient-to-r from-red-700 to-blue-800 flex justify-center">
+                    <h2 className="text-white text-5xl p-8 text-center font-semibold">Keynote Speaker</h2>
+                </div>
             </div>
         </header>
     )
