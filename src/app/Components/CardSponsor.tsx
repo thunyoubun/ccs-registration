1b2703bde429f7aa1274cc85a1d9515b5e50b496
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-05-04 14:09:06 +0700

    ปรับแต่ง Footer sponsor

--- src/app/Components/CardSponsor.tsx
+++ src/app/Components/CardSponsor.tsx
@@ -5,8 +5,8 @@ function CardSponsor({ path, nameCompany1, nameCompany2}:{ path:string, nameComp
         <div className="max-w-sm lg:p-6 bg-white grid justify-items-center">
             <Image src={path} alt={nameCompany1} width={128} height={128}/>
             <div className="text-center">
-                <h5 className="text-xl font-medium">{nameCompany1}</h5>
-                <h5 className="text-base font-light">{nameCompany2}</h5>
+                <h5 className="text-base font-medium">{nameCompany1}</h5>
+                <h5 className="text-sm font-light">{nameCompany2}</h5>
             </div>
         </div>
     )
