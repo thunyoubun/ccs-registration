e9d067d7a47d06e7753f63ca6279e51822e5da03
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-05-05 22:00:55 +0700

    change color

--- src/app/Components/TextInput.tsx
+++ src/app/Components/TextInput.tsx
@@ -9,7 +9,7 @@ function TextInput(fnc: Function, word: string, tagName: string, typeInput: stri
                     type={typeInput} 
                     id={tagName}
                     {...fnc(tagName)}
-                    className=" w-full p-1 rounded-md border-0 shadow-sm ring-1 ring-inset ring-rose-700 focus:ring-2 focus:ring-blue-900"
+                    className=" w-full p-1 rounded-md border-0 shadow-sm ring-1 ring-inset ring-teal-500 focus:ring-2 focus:ring-blue-950"
                     required
                     />
             </div>
