1f978695c62ac680090fb0df569e931403735b73
neabparinya11 <76035451+neabparinya11@users.noreply.github.com>
2024-05-30 13:35:08 +0700

    add height

--- src/app/Components/RegistrationMap.tsx
+++ src/app/Components/RegistrationMap.tsx
@@ -18,7 +18,7 @@ function RegistrationMap() {
           1 - 2, Novotel Bangkok Future Park Rangsit, Bangkok, Thailand
         </h1>
       </div>
-      <div className=" flex-grow justify-center relative">
+      <div className=" flex-grow justify-center relative h-96">
         <iframe
           title="06Map"
           src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15485.547317181567!2d100.6167955!3d13.9950625!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30de1168e62e2101%3A0x9da7a57659ec3960!2z4LmC4LiZ4LmC4Lin4LmA4LiX4LilIOC4geC4o-C4uOC4h-C5gOC4l-C4niDguJ_guLTguKfguYDguIjguK3guKPguYzguJ7guLLguKPguYzguIQg4Lij4Lix4LiH4Liq4Li04LiV!5e0!3m2!1sth!2sth!4v1712834003533!5m2!1sth!2sth"
@@ -27,16 +27,6 @@ function RegistrationMap() {
           className="w-full md:h-full h-80"
         ></iframe>
       </div>
-      {/* <div className="text-center p-2">
-        <h3 className="">
-          <span className="font-semibold">
-            {!secondDate ? "6 - 7 June" : "7 June"}
-          </span>
-          {!secondDate
-            ? ", at Novotel Bangkok Future Park Rangsit, Bangkok, Thailand"
-            : ", at Chiang Mai University, Chiang Mai, Thailand"}
-        </h3>
-      </div> */}
     </div>
   );
 }
